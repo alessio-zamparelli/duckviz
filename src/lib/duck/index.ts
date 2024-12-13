27e70321d6be81c2fd23c139aa8d52ca7111cc3a
alessio-zamparelli <alessio.zamparelli@gmail.com>
2024-12-13 20:32:21 +0100

    build: moved bundle out of build

--- src/lib/duck/index.ts
+++ src/lib/duck/index.ts
@@ -3,29 +3,39 @@
 // export * from "./platform_provider"
 // export * from "./resolvable"
 
-import * as duckdb from "@duckdb/duckdb-wasm"
-import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url"
-import mvp_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url"
-import duckdb_wasm_next from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url"
-import eh_worker from "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url"
+import * as duckdb from '@duckdb/duckdb-wasm';
+// import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
+// import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
+// import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
+// import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
-const MANUAL_BUNDLES = {
-	mvp: {
-		mainModule: duckdb_wasm,
-		mainWorker: mvp_worker
-	},
-	eh: {
-		mainModule: duckdb_wasm_next,
-		// mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js', import.meta.url).toString(),
-		mainWorker: eh_worker
-	}
-}
+// const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+// 	mvp: {
+// 			mainModule: duckdb_wasm,
+// 			mainWorker: mvp_worker,
+// 	},
+// 	eh: {
+// 			mainModule: duckdb_wasm_eh,
+// 			mainWorker: eh_worker,
+// 	},
+// };
 
 // Select a bundle based on browser checks
-const bundle = await duckdb.selectBundle(MANUAL_BUNDLES)
+// const bundle = await duckdb.selectBundle(MANUAL_BUNDLES)
+
+
+const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+
+// Select a bundle based on browser checks
+const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+const worker_url = URL.createObjectURL(
+  new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
+);
 
 // Instantiate the asynchronus version of DuckDB-wasm
-const worker = new Worker(bundle.mainWorker!)
+// const worker = new Worker(bundle.mainWorker!)
+const worker = new Worker(worker_url);
 const logger = new duckdb.ConsoleLogger()
 export const db = new duckdb.AsyncDuckDB(logger, worker)
 await db.instantiate(bundle.mainModule, bundle.pthreadWorker)
