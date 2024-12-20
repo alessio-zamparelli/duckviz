7964924b4ff5b2bc278e17da7affcfc87dceda72
alessio-zamparelli <alessio.zamparelli@gmail.com>
2024-12-20 23:10:09 +0100

    fix: vite error

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
 import commonjs from "vite-plugin-commonjs"
-import { VitePluginRadar } from "vite-plugin-radar"
-
 import path from "path"
 
 // https://vite.dev/config/
@@ -20,7 +18,7 @@ export default defineConfig({
       filter(id) {
         // `node_modules` is exclude by default, so we need to include it explicitly
         // https://github.com/vite-plugin/vite-plugin-commonjs/blob/v0.7.0/src/index.ts#L125-L127
-        
+
         if (id.includes("node_modules/plotly.js")) return true
         if (id.includes("node_modules/elementary-circuits-directed-graph")) return true
       },
